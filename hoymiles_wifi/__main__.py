4461d4e875bcdae95ef5c1e9d8ba71a49f7ec087
suaveolent <suaveolent@users.noreply.github.com>
2023-12-19 09:28:47 +0100

    realdatanew and getconfig working

--- hoymiles_wifi/__main__.py
+++ hoymiles_wifi/__main__.py
@@ -1,5 +1,5 @@
 import argparse
-from hoymiles_wifi.inverter2 import Inverter2
+from hoymiles_wifi.inverter import Inverter
 import time
 
 def main():
@@ -7,17 +7,28 @@ def main():
     parser.add_argument("ip_address", type=str, help="IP address or hostname of the inverter")
     args = parser.parse_args()
 
-    inverter = Inverter2(args.ip_address)
+    inverter = Inverter(args.ip_address)
 
     try:
         while True:
 
+            response = inverter.get_real_data_new()
+            if response:
+                print(f"Get Real Data New Response: {response}")
+            else:
+                print("Unable to retrieve real data new")
+
             response = inverter.get_real_data_hms()
+            if response:
+                print(f"Get Real Data HMS Response: {response}")
+            else:
+                print("Unable to retrieve real data hms")
+
+            response = inverter.get_real_data()
             if response:
                 print(f"Get Real Data Response: {response}")
             else:
                 print("Unable to retrieve real data")
-
 
             response = inverter.get_config()
             if response:
--- hoymiles_wifi/inverter.py
+++ hoymiles_wifi/inverter.py
@@ -1,72 +1,107 @@
-# import socket
-# import struct
-# from hoymiles_wifi import logger
-# from hoymiles_wifi.protos.RealData_pb2 import RealDataResDTO, HMSStateResponse
+import socket
+import struct
+from hoymiles_wifi import logger
+from crcmod import mkCrcFun
+from datetime import datetime
+import time
+import warnings
 
-# from crcmod import mkCrcFun
-# from datetime import datetime
-# import time
+from hoymiles_wifi.protobuf import (
+    GetConfig_pb2,
+    RealData_pb2,
+    RealDataNew_pb2,
+    RealDataHMS_pb2,
+ )
 
+from hoymiles_wifi.const import (
+    INVERTER_PORT, 
+    CMD_HEADER,
+    CMD_GET_CONFIG,
+    CMD_REAL_RES_DTO,
+    CMD_REAL_DATA_RES_DTO,
+)
 
-# INVERTER_PORT = 10081
+class NetworkState:
+    Unknown = 0
+    Online = 1
+    Offline = 2
 
-# class NetworkState:
-#     Unknown = 0
-#     Online = 1
-#     Offline = 2
+class Inverter:
+    def __init__(self, host):
+        self.host = host
+        self.state = NetworkState.Unknown
+        self.sequence = 0
 
-# class Inverter:
-#     def __init__(self, host):
-#         self.host = host
-#         self.state = NetworkState.Unknown
-#         self.sequence = 0
+    def set_state(self, new_state):
+        if self.state != new_state:
+            self.state = new_state
+            logger.info(f"Inverter is {new_state}")
 
-#     def set_state(self, new_state):
-#         if self.state != new_state:
-#             self.state = new_state
-#             logger.info(f"Inverter is {new_state}")
-
-#     def update_state(self):
-#         self.sequence = (self.sequence + 1) & 0xFFFF
-
-#         request = RealDataResDTO()
-#         # date = datetime.now()
-#         # time_string = date.strftime("%Y-%m-%d %H:%M:%S")
-#         # request.ymd_hms = time_string
-#         # request.cp = 23 + self.sequence
-#         # request.offset = 0
-#         # request.time = int(time.time())
-#         header = b"\x48\x4d\xa3\x03"
-#         request_as_bytes = request.SerializeToString()
-#         crc16 = mkCrcFun(0x18005, rev=True, initCrc=0xFFFF, xorOut=0x0000)(request_as_bytes)
-#         length = len(request_as_bytes) + 10
-
-#         # compose request message
-#         message = header + struct.pack('>HHHH', self.sequence, crc16, length, 0) + request_as_bytes
-
-#         address = (self.host, INVERTER_PORT)
-#         try:
-#             with socket.create_connection(address, timeout=0.5) as stream:
-#                 stream.settimeout(5)
-#                 stream.sendall(message)
-#                 buf = stream.recv(1024)
-#         except (socket.error, socket.timeout) as e:
-#             logger.debug(f"{e}")
-#             self.set_state(NetworkState.Offline)
-#             return None
-
-#         read_length = struct.unpack('>H', buf[6:8])[0]
-
-#         try:
-#             parsed = HMSStateResponse.FromString(buf[10:10+read_length])
-
-#             if not parsed:
-#                 raise ValueError("Parsing resulted in an empty or falsy value")
+    def update_state(self):
+        warnings.warn("This function is deprecated and will be removed in the future.", FutureWarning)
+        return self.get_real_data_hms()
         
-#         except Exception as e:
-#             logger.debug(f"Failed to parse response: {e}")
-#             self.set_state(NetworkState.Offline)
-#             return None
+    def get_real_data_hms(self):
+        request = RealDataHMS_pb2.HMSRealDataResDTO()
+        command = CMD_REAL_DATA_RES_DTO
+        return self.send_request(command, request, RealDataHMS_pb2.HMSStateResponse)
+    
+    def get_real_data(self):
+        request = RealData_pb2.RealDataResDTO()
+        command = CMD_REAL_DATA_RES_DTO
+        return self.send_request(command, request, RealData_pb2.RealDataReqDTO)
+    
 
-#         self.set_state(NetworkState.Online)
-#         return parsed
+    def get_real_data_new(self):
+        request = RealDataNew_pb2.RealDataNewResDTO()
+        request.timestamp_ymd_hms = datetime.now().strftime("%Y-%m-%d %H:%M:%S").encode("utf-8")
+        request.offset = 28800
+        request.timestamp = int(time.time())
+        command = CMD_REAL_RES_DTO
+        return self.send_request(command, request, RealDataNew_pb2.RealDataNewReqDTO)
+    
+    def get_config(self):
+        request = GetConfig_pb2.GetConfigResDTO()
+        request.offset = 28800
+        request.time = int(time.time()) - 60
+        command = CMD_GET_CONFIG
+        return self.send_request(command, request, GetConfig_pb2.GetConfigReqDTO)    
+
+    def send_request(self, command, request, response_type):
+        self.sequence = (self.sequence + 1) & 0xFFFF
+
+        request_as_bytes = request.SerializeToString()
+        crc16 = mkCrcFun(0x18005, rev=True, initCrc=0xFFFF, xorOut=0x0000)(request_as_bytes)
+        length = len(request_as_bytes) + 10
+
+        # compose request message
+        header = CMD_HEADER + command
+        message = header + struct.pack('>HHH', self.sequence, crc16, length) + request_as_bytes
+
+        address = (self.host, INVERTER_PORT)
+        try:
+            with socket.create_connection(address, timeout=0.5) as stream:
+                stream.settimeout(5)
+                stream.sendall(message)
+                buf = stream.recv(1024)
+        except (socket.error, socket.timeout) as e:
+            logger.error(f"{e}")
+            self.set_state(NetworkState.Offline)
+            return None
+
+        read_length = struct.unpack('>H', buf[6:8])[0]
+
+        try:
+            parsed = response_type.FromString(buf[10:10 + read_length])
+
+            if not parsed:
+                raise ValueError("Parsing resulted in an empty or falsy value")
+
+        except Exception as e:
+            logger.debug(f"Failed to parse response: {e}")
+            self.set_state(NetworkState.Offline)
+            return None
+
+        self.set_state(NetworkState.Online)
+        return parsed
+
