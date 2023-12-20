93ad5c1717c6e2756355bda1a741aebf46f733d3
suaveolent <suaveolent@users.noreply.github.com>
2023-12-20 09:32:26 +0100

    restructure

--- hoymiles_wifi/__main__.py
+++ hoymiles_wifi/__main__.py
@@ -2,6 +2,30 @@ import argparse
 from hoymiles_wifi.inverter import Inverter
 import time
 
+def get_real_data_new(inverter):
+    return inverter.get_real_data_new()
+
+def get_real_data_hms(inverter):
+    return inverter.get_real_data_hms()
+
+def get_real_data(inverter):
+    return inverter.get_real_data()
+
+def get_config(inverter):
+    return inverter.get_config()
+
+def network_info(inverter):
+    return inverter.network_info()
+
+def app_information_data(inverter):
+    return inverter.app_information_data()
+
+def app_get_hist_power(inverter):
+    return inverter.app_get_hist_power()
+
+def print_invalid_command(command):
+    print(f"Invalid command: {command}")
+
 def main():
     parser = argparse.ArgumentParser(description="Hoymiles HMS Monitoring")
     parser.add_argument("--host", type=str, required=True, help="IP address or hostname of the inverter")
@@ -10,20 +34,25 @@ def main():
 
     inverter = Inverter(args.host)
 
-    try:
-        while True:
-            # Execute the specified command
-            response = getattr(inverter, args.command, lambda: None)()
-            
-            if response:
-                print(f"{args.command.capitalize()} Response: {response}")
-            else:
-                print(f"Unable to retrieve {args.command.replace('_', ' ')}")
+    # Execute the specified command using a switch case
+    switch = {
+        'get-real-data-new': get_real_data_new,
+        'get-real-data-hms': get_real_data_hms,
+        'get-real-data': get_real_data,
+        'get-config': get_config,
+        'network-info': network_info,
+        'app-information-data': app_information_data,
+        'app-get-hist-power': app_get_hist_power,
+    }
 
-            # Sleep for a while before the next update
-            time.sleep(60)
-    except KeyboardInterrupt:
-        print("Exiting.")
+    command_func = switch.get(args.command, print_invalid_command)
+    response = command_func(inverter)
+
+    if response:
+        print(f"{args.command.capitalize()} Response: {response}")
+    else:
+        print(f"Unable to retrieve {args.command.replace('_', ' ')}")
+
 
 if __name__ == "__main__":
     main()
