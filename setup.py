4c865164f8b07dc8c2ef33cfae58b11fe7b89ffe
suaveolent <suaveolent@users.noreply.github.com>
2024-02-15 10:11:16 +0100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name='hoymiles-wifi',
     packages=['hoymiles_wifi', 'hoymiles_wifi.protobuf'],
-    version='0.1.3',
+    version='0.1.4',
     description='A python library for interfacing with Hoymiles HMS-XXXXW-T2 series of micro-inverters.',
     author='suaveolent',
     include_package_data=True,
