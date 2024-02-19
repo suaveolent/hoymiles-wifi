66a8f91713da8ca3904b7b1effac49074172ce5e
suaveolent <suaveolent@users.noreply.github.com>
2024-02-19 08:57:38 +0100

    fix required dependency installs

--- setup.py
+++ setup.py
@@ -3,7 +3,11 @@ from setuptools import setup
 setup(
     name='hoymiles-wifi',
     packages=['hoymiles_wifi', 'hoymiles_wifi.protobuf'],
-    version='0.1.4',
+    install_requires=[
+        'protobuf',
+        'crcmod',
+    ],
+    version='0.1.5',
     description='A python library for interfacing with Hoymiles HMS-XXXXW-T2 series of micro-inverters.',
     author='suaveolent',
     include_package_data=True,
