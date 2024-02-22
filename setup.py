e6b0d027bedd6d822e327216c9e81639d3239bb0
suaveolent <suaveolent@users.noreply.github.com>
2024-02-22 12:20:32 +0100

    bump version to 0.1.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
         'protobuf',
         'crcmod',
     ],
-    version='0.1.5',
+    version='0.1.6',
     description='A python library for interfacing with Hoymiles HMS-XXXXW-T2 series of micro-inverters.',
     author='suaveolent',
     include_package_data=True,
